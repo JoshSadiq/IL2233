c3ac499837abee525923399ba9b8a011b12caca1
Ludvig Larsson <ludvig19@icloud.com>
2023-05-16 12:53:41 +0200

    kmeans done and tested

--- 3-lab/k-means.c
+++ 3-lab/k-means.c
@@ -18,7 +18,7 @@ void displayTimeSeries(double *data, int samples, int observations) {
         for (int o = 0; o < observations; o++) {
             if (o % 8 == 0 && o != 0)
                 printf("\n");
-            printf("%f ", data[s * observations + o]);
+            printf("%.2f ", data[s * observations + o]);
         }
         printf("\n");
     }
@@ -30,7 +30,7 @@ void displayCentroids(int clusters, int observations, double centroids[][observa
         for (int o = 0; o < observations; o++) {
             if (o % 8 == 0 && o != 0)
                 printf("\n");
-            printf("%f ", centroids[c][o]);
+            printf("%.2f ", centroids[c][o]);
         }
         printf("\n");
     }
@@ -65,6 +65,68 @@ double distance(double *series, double *centroid, int observations) {
     return dist;
 }
 
+void initCentroids(double *centroids, double *data, int clusters, int samples, int observations) {
+    // to avoid duplicate initial centroids
+    int sampleNoInCentroids[clusters];
+    for (int i = 0; i < clusters; i++)
+        sampleNoInCentroids[i] = -1;
+
+    // randomly select initial centroids (random samples)
+    int randomSampleNo;
+    for (int c = 0; c < clusters; c++) {
+        while (isAlreadyCentroid(randomSampleNo = rand() % samples, sampleNoInCentroids, clusters));
+        sampleNoInCentroids[c] = randomSampleNo;
+        memcpy(&centroids[c], &data[randomSampleNo], observations * sizeof(double));
+    }
+}
+
+void updateCentroids(double *centroids, double *data, int *clusterAssignments, int clusters, int samples, int observations) {
+    for (int c = 0; c < clusters; c++) {
+        int samplesInCluster[samples]; // at most all samples
+        int clusterSize = 0;
+        
+        // find which samples assigned to the cluster
+        for (int s = 0; s < samples; s++)
+            if (clusterAssignments[s] == c)
+                samplesInCluster[clusterSize++] = s;
+
+        // for (int i = 0; i < clusterSize; i++)
+        //     printf("Found mapping: TS %d -> C %d\n", samplesInCluster[i]+1, c+1);
+
+        // average of observations at each time step
+        for (int o = 0; o < observations; o++) {
+            double sum = 0.0;
+            for (int s = 0; s < clusterSize; s++)
+                sum += data[samplesInCluster[s] * observations + o];
+            centroids[c * observations + o] = sum / clusterSize;
+        }
+    }
+}
+
+double updateAssignments(double *centroids, double *data, int *clusterAssignments, int clusters, int samples, int observations) {
+    double cost = 0.0;
+    // re-assign samples to the cluster whose centroid is closest
+    for (int s = 0; s < samples; s++) {
+        double minDistance = DBL_MAX;
+        int closestCluster = clusterAssignments[s];
+        // printf("Sample %d closest to %d\n", s+1, closestCluster);
+        
+        for (int c = 0; c < clusters; c++) {
+            double currentDistance = 
+                distance(&data[s * observations], &centroids[c], observations);
+            if (currentDistance < minDistance) {
+                minDistance = currentDistance;
+                closestCluster = c;
+            }
+        }
+        // printf("Sample %d closest to %d\n", s+1, closestCluster);
+        cost += minDistance;
+        clusterAssignments[s] = closestCluster;
+    }
+
+    return cost;
+}
+
 // ORIGINAL: void kmeans(int *assignment, int K, int max_iter, int n_samples, int m_features, double *data)
 // assignment -> clusterAssignments (symbol table)
 // K -> clusters
@@ -72,21 +134,9 @@ double distance(double *series, double *centroid, int observations) {
 // m_features -> observations
 void kmeans(double *data, int *clusterAssignments, int max_iter, int clusters, int samples, int observations) {
     double *centroids = malloc(clusters * observations * sizeof(double));
-    double previousCost = 0.0;
+    double newCost, previousCost = 0.0;
 
-    // to avoid duplicate initial centroids
-    int sampleNoInCentroids[clusters];
-    for (int i = 0; i < clusters; i++)
-        sampleNoInCentroids[i] = -1;
-
-    // randomly select initial centroids (random samples)
-    for (int c = 0; c < clusters; c++) {
-        int randomSample;
-        while (isAlreadyCentroid(randomSample = rand() % samples, sampleNoInCentroids, clusters));
-        sampleNoInCentroids[c] = randomSample;
-        memcpy(&centroids[c], &data[randomSample], observations * sizeof(double));
-    }
-
+    initCentroids(centroids, data, clusters, samples, observations);
     // displayCentroids(clusters, observations, centroids);
     
     // assign samples to random clusters
@@ -94,70 +144,98 @@ void kmeans(double *data, int *clusterAssignments, int max_iter, int clusters, i
         clusterAssignments[s] = rand() % clusters;
 
     // display initial cluster assignments
-    // for (int s = 0; s < samples; s++) {
+    // for (int s = 0; s < samples; s++)
     //     printf("Initial mapping: TS %d -> C %d\n", s+1, clusterAssignments[s]+1);
-    // }
 
     for (int iter = 0; iter < max_iter; iter++) {
-        // update each cluster's centroid
-        for (int c = 0; c < clusters; c++) {
-            int samplesInCluster[samples];
-            int clusterSize = 0;
-            
-            // find the samples belonging to the cluster
-            for (int s = 0; s < samples; s++)
-                if (clusterAssignments[s] == c)
-                    samplesInCluster[clusterSize++] = s;
-
-            // display found samples in cluster
-            // for (int i = 0; i < clusterSize; i++)
-            //     printf("Found mapping: TS %d -> C %d\n", samplesInCluster[i]+1, c+1);
-
-            // calculate new centroids by summing observation values for each
-            // sample at each time step then dividing by the number of samples
-            for (int o = 0; o < observations; o++) {
-                double sum = 0.0;
-                for (int s = 0; s < clusterSize; s++)
-                    sum += data[samplesInCluster[s] * observations + o];
-                centroids[c * observations + o] = sum / clusterSize;
-            }
-        }
-        
-        // displayCentroids(clusters, observations, centroids);
-
-        double newCost = 0.0;
-        // re-assign samples to the cluster whose centroid is closest
-        for (int s = 0; s < samples; s++) {
-            double minDistance = DBL_MAX;
-            int closestCluster = clusterAssignments[s];
-            // printf("Sample %d closest to %d\n", s+1, closestCluster);
-            
-            for (int c = 0; c < clusters; c++) {
-                double currentDistance = 
-                    distance(&data[s * observations], &centroids[c], observations);
-                if (currentDistance < minDistance) {
-                    minDistance = currentDistance;
-                    closestCluster = c;
-                }
-            }
-            // printf("Sample %d closest to %d\n", s+1, closestCluster);
-            newCost += minDistance;
-            clusterAssignments[s] = closestCluster;
-        }
+        newCost = 
+            updateAssignments(centroids, data, clusterAssignments, clusters, samples, observations);
 
         printf("Cost: %f\n", newCost);
-        if (fabs(newCost - previousCost) == 0) {
+        if (fabs(newCost - previousCost) < 1.0) {
             printf("Converged after %d iterations\n", iter+1);
             break;
         } else {
             previousCost = newCost;
         }
+
+        updateCentroids(centroids, data, clusterAssignments, clusters, samples, observations);
+        // displayCentroids(clusters, observations, centroids);
     }
 
     free(centroids);
 }
 
+
+bool testImpl() {
+    double data[3][6] = {
+        {1.0, 2.0, 1.0, 2.0, 1.0, 2.0},
+        {8.0, 9.0, 8.0, 9.0, 8.0, 9.0},
+        {15.0, 14.0, 15.0, 14.0, 15.0, 14.0}};
+    int samples = 3;
+    int observations = 6;
+    int clusters = 2;
+    double centroids[clusters][observations];
+    int clusterAssignments[samples];
+
+    memcpy(centroids[0], data[0], observations * sizeof(double));
+    memcpy(centroids[1], data[2], observations * sizeof(double));
+
+    printf("Initial data:\n");
+    displayTimeSeries(&data[0][0], samples, observations);
+
+    printf("\nInitial centroids:\n");
+    displayCentroids(clusters, observations, centroids);
+
+    // "random assignments"
+    clusterAssignments[0] = 1; // sample 1 -> cluster 2 (sample 3)
+    clusterAssignments[1] = 1; // sample 2 -> cluster 2 (sample 3)
+    clusterAssignments[2] = 0; // sample 3 -> cluster 1 (sample 1)
+
+    printf("\nInitial 'random' mapping:\n");
+    for (int i = 0; i < samples; i++)
+        printf("time series %d -> cluster %d\n", i+1, clusterAssignments[i]+1);
+
+    // test cost function
+    double expectedCost = 7 + 7 + 7 + 7 + 7 + 7;
+    double actualCost = distance(data[1], centroids[0], observations);
+    if (expectedCost != actualCost) {
+        printf("Cost(distance) between sample 2 and cluster 1 is %f, expected %f\n", actualCost, expectedCost);
+        return false;
+    } else {
+        printf("\n- Cost is working correctly\n");
+        printf("Cost of sample 2 to cluster 1 is %.2f\n", actualCost);
+    }
+
+    updateAssignments(&centroids[0][0], &data[0][0], clusterAssignments, clusters, samples, observations);
+    if (clusterAssignments[0] != 0 || clusterAssignments[1] != 1 || clusterAssignments[2] != 1) {
+        return false;
+    } else {
+        printf("\n- Cluster assignments are working correctly, updated:\n");
+        for (int i = 0; i < samples; i++)
+            printf("time series %d -> cluster %d\n", i+1, clusterAssignments[i]+1);
+    }
+
+
+    updateCentroids(&centroids[0][0], &data[0][0], clusterAssignments, clusters, samples, observations);
+    double expectedCentroid[6] = {11.5, 11.5, 11.5, 11.5, 11.5, 11.5};
+    for (int i = 0; i < observations; i++)
+        if (expectedCentroid[i] != centroids[1][i]) {
+            printf("centroid 1, feature %d: %.2f, expected %.2f\n", i+1, centroids[0][i], expectedCentroid[i]);
+            return false;
+        }
+    printf("\n- Centroids are updated correctly, now they are:\n");
+    displayCentroids(clusters, observations, centroids);
+
+    printf("\n");
+
+    return true;
+}
+
 int main(int argc, char *argv[]) {
+    if (testImpl() == false)
+        return 1;
+
     if (argc != 3 && argc != 5) {
         printf("Usage: %s rand/iris/bme clusters [samples] [observations]\n", argv[0]);
         return 1;
